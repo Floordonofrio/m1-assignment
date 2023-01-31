fd31e820faa6f0fddd76f529922241d24eced715
Florencia D'Onofrio <flordonofrio2002@gmail.com>
2023-01-30 18:34:33 -0800

    text-photos

--- js/gallery.js
+++ js/gallery.js
@@ -5,11 +5,13 @@ var image; //store the assembled image list codes
 var openList = "<li class='photo'>";
 var closeList = "</li>";
 //Lab assignment
-//var openCaptionTag = ;
-//var cloaseCaptionTag =  ;
+var openCaptionTag = "<p id='paragraph-below'";
+var closeCaptionTag = "</p>" ;
 //var openDescTag = ;
 //var closeDescTag = ;
-//captionTexts = ''
+captionTexts = ["Picture1", "Picture2", "Picture3","Picture4", "Picture5", "Picture6","Picture7", "Picture8", "Picture9","Picture10"];
+text = [];
+textList = [];
 //descTexts = ''
 
 //Create a loop to create 6 images satarting with index of 0
@@ -21,3 +23,9 @@ for (var i=0; i<10; i++){
 }
 //Display all six image codes store in the array
 document.getElementById("gallery").innerHTML = imageList.join("");
+
+for (var i=0; i<10; i++){
+    text = openCaptionTag + captionTexts[i] + closeCaptionTag;
+    textList.push(text);
+}
+document.getElementById("gallery").innerHTML = textList.join("");
