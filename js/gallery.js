d83538cedf8e9411a4b7494b5dcec66200a8058b
Florencia D'Onofrio <flordonofrio2002@gmail.com>
2023-01-31 18:28:49 -0800

    description photos-hover

--- js/gallery.js
+++ js/gallery.js
@@ -7,17 +7,21 @@ var closeList = "</li>";
 //Lab assignment
 var openCaptionTag = "<p id='paragraph-below'>";
 var closeCaptionTag = "</p>" ;
-captionTexts = ["Tom and Jerry", "Rufus", "Fiona","Bimbo", "Emma", "Blue","Picture7", "Picture8", "Picture9","Picture10"];
+captionTexts = ["Tom and Jerry", "Rufus", "Fiona","Bimbo", "Emma", "Blue","Butter", "Orange", "Elmo","Black"];
 text = [];
 textList = [];
 wholeText_Imagelist = [];
+//Hover
+openHover = "<div class='hoverimage'><p id='paragraph-hover'>"
+closeHover = "</p></div>"
+hoverText = ["Hi! we are friendly and like to play", "I like to take looong naps","I like playing with my toys and with humans too", "I know I am tiny but I have a lot of energy","I am a little bit old but I will be good company","I like starying at people for long hours","Hi, I am Butter, I do not like my name very much, I think it does not represent my intelectuality","I like playing with humans, cats, dogs, everything","I like when humans pet me", "Hi I am Black, and I like to sleep, a lot"]
 
 
 //Create a loop to create 6 images satarting with index of 0
 for (var i=0; i<10; i++){
     fileNames.push("dog" + (i+1)); //creat image files names ans store in the array
     photos.push("<img src='images/" + fileNames[i] + ".jpg' width=250 height=250'>" ); //assembly file naes and store it in the array
-    image = openList + photos[i] + openCaptionTag + captionTexts[i] + closeCaptionTag +closeList; //assembly image element and store in variable
+    image = openList + photos[i] + openCaptionTag + captionTexts[i] + closeCaptionTag + openHover+ hoverText[i] + closeHover + closeList; //assembly image element and store in variable
     imageList.push(image);
 }
 //Display all six image codes store in the array
